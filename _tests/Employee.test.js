a671669bc76eda11325794571cfcd56bebbd190e
aali9 <74592306+aali9@users.noreply.github.com>
2022-06-26 17:57:13 +0100

    changes

---
+++ _tests/Employee.test.js
@@ -0,0 +1,22 @@
+const Employee = require ('../lib/Employee');
+
+
+describe("Creates an Employee", () => {
+    it ("Employee name should get printed", () => {
+        const name = "Anfaal";
+        const Emp = new Employee(name);
+        expect(Emp.name).toBe(name);
+    })
+
+    it ("Employee id gets printed", () => {
+        const id = "123";
+        const Emp = new Employee("Anfaal", id);
+        expect(Emp.id).toBe(id);
+    })
+
+    it ("Employee email gets printed", () => {
+        const email = "anfaalali@gmail.com";
+        const Emp = new Employee("Anfaal", "123", email);
+        expect(Emp.email).toBe(email);
+    })
+})
